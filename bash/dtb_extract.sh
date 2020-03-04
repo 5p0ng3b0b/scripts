5a935cde6b1df225e3250707e74ab8072c19ffff
5p0ng3b0b <12113595+5p0ng3b0b@users.noreply.github.com>
2020-03-04 14:47:58 +0000

    Update dtb_extract.sh

--- bash/dtb_extract.sh
+++ bash/dtb_extract.sh
@@ -1,9 +1,8 @@
 #!/system/bin/sh
 ##################################################
 #
-# Extract DTB from boot.img or boot.PARTITION file
+# Extract DTB from boot.img
 # 2020/03/02 5p0ng3b0b
-# Does not require busybox
 #
 ##################################################
 if [ -z $@ ]; then echo "Usage: $(basename $0) [image file]"; exit; fi
@@ -15,7 +14,7 @@ if [ ! -e $IMG_FILE ]; then echo "Could not open file ${IMG_FILE}, giving up."; 
 if [ ! $(dd if=$IMG_FILE bs=1 count=$BOOT_MAGIC_SIZE 2>/dev/null) = $BOOT_MAGIC ]; then echo "Android magic not found, giving up"; exit; fi
 
 # Get device board ID
-ID=$(printf $(tr -d '\0' </proc/device-tree/model)-$(tr -d '\0' </proc/device-tree/amlogic-dt-id) | sed 's/_/-/g' | tr '[:upper:]' '[:lower:]')
+#ID=$(printf $(tr -d '\0' </proc/device-tree/model)-$(tr -d '\0' </proc/device-tree/amlogic-dt-id) | sed 's/_/-/g' | tr '[:upper:]' '[:lower:]')
 
 # Start extraction
 PAGE_SIZE=$((16#$(dd if=$IMG_FILE bs=1 skip=36 count=$BYTES 2>/dev/null | od -tx1 | head -n1 | cut -d ' ' -f 2- | awk '{print $4$3$2$1}')))
@@ -31,8 +30,13 @@ SECOND_OFFSET=$(($RAMDISK_OFFSET + ($m * $PAGE_SIZE)))
 if [ -z "$SECOND_OFFSET" ]; then echo "Could not find second stage boot image, giving up."; exit; fi
 dd if="$IMG_FILE" of="${IMG_FILE}-second.gz" bs=1 skip=$SECOND_OFFSET count=$SECOND_SIZE 2>/dev/null
 gunzip -f "${IMG_FILE}-second.gz"
-DTB_OFFSET=$(printf $(($(od -v -t x1 "${IMG_FILE}-second" | sed 's/[^ ]* *//' | tr '\012' ' ' | sed 's/d0 0d fe ed/FOUND\n&/g' | tr ' ' '\012' | grep -n FOUND | awk -F: '{print $1}' 2>/dev/null | head -n1) - 1)))
-DTB_LEN=$((16#$(dd if="${IMG_FILE}-second" bs=1 skip=$(($DTB_OFFSET + 4)) count=4 2>/dev/null | od -tx1 | head -n 1 | cut -d ' ' -f 2- | sed 's/ //g' )))
-dd if="${IMG_FILE}-second" of="${ID}.dtb" bs=1 skip=$DTB_OFFSET count=$DTB_LEN 2>/dev/null
+DTB_LIST=$(od -Ad -tx1 ${IMG_FILE}-second | grep 'd0 0d fe ed' | awk '{print $1}')
+if [ -z "$DTB_LIST" ]; then echo "No DTB headers found, giving up"; exit; fi
+for DTB_OFFSET in $DTB_LIST; do
+    DTB_LEN=$((16#$(dd if="${IMG_FILE}-second" bs=1 skip=$(($DTB_OFFSET + 4)) count=4 2>/dev/null | od -tx1 | head -n 1 | cut -d ' ' -f 2- | sed 's/ //g' )))
+    ID=$(dd if="${IMG_FILE}-second" bs=1 skip=$(($DTB_OFFSET + 96)) count=16 2>/dev/null | sed 's#\x0##g')
+    dd if="${IMG_FILE}-second" of="${ID}.dtb" bs=1 skip=$DTB_OFFSET count=$DTB_LEN 2>/dev/null
+    echo "Extracted: ${ID}.dtb Size: $(du ${ID}.dtb | awk '{print $1}')Kb Offset: $DTB_OFFSET Length: $DTB_LEN"
+    done
 rm ${IMG_FILE}-second
-echo "Extracted: ${ID}.dtb Size: $(du ${ID}.dtb | awk '{print $1}')Kb"
+
