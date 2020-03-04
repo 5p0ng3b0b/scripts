e79e551121645da11cf313146be66dcc30ffd3a9
5p0ng3b0b <12113595+5p0ng3b0b@users.noreply.github.com>
2020-03-04 15:38:22 +0000

    Update dtb_extract.sh

--- bash/dtb_extract.sh
+++ bash/dtb_extract.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 ##################################################
 #
-# Extract DTB from boot.img
-# 2020/03/02 5p0ng3b0b
+# Extract DTB from boot.img or boot.PARTITION
+# 2020/03/04 5p0ng3b0b
 #
 ##################################################
 if [ -z $@ ]; then echo "Usage: $(basename $0) [image file]"; exit; fi
@@ -14,8 +14,7 @@ if [ ! -e $IMG_FILE ]; then echo "Could not open file ${IMG_FILE}, giving up."; 
 if [ ! $(dd if=$IMG_FILE bs=1 count=$BOOT_MAGIC_SIZE 2>/dev/null) = $BOOT_MAGIC ]; then echo "Android magic not found, giving up"; exit; fi
 
 # Get device board ID
-#ID=$(printf $(tr -d '\0' </proc/device-tree/model)-$(tr -d '\0' </proc/device-tree/amlogic-dt-id) | sed 's/_/-/g' | tr '[:upper:]' '[:lower:]')
-
+BOARD_ID=$(tr -d '\0' </proc/device-tree/amlogic-dt-id)
 # Start extraction
 PAGE_SIZE=$((16#$(dd if=$IMG_FILE bs=1 skip=36 count=$BYTES 2>/dev/null | od -tx1 | head -n1 | cut -d ' ' -f 2- | awk '{print $4$3$2$1}')))
 KERNEL_SIZE=$((16#$(dd if=$IMG_FILE bs=1 skip=8 count=$BYTES 2>/dev/null | od -tx1 | head -n1 | cut -d ' ' -f 2- | awk '{print $4$3$2$1}')))
@@ -35,8 +34,10 @@ if [ -z "$DTB_LIST" ]; then echo "No DTB headers found, giving up"; exit; fi
 for DTB_OFFSET in $DTB_LIST; do
     DTB_LEN=$((16#$(dd if="${IMG_FILE}-second" bs=1 skip=$(($DTB_OFFSET + 4)) count=4 2>/dev/null | od -tx1 | head -n 1 | cut -d ' ' -f 2- | sed 's/ //g' )))
     ID=$(dd if="${IMG_FILE}-second" bs=1 skip=$(($DTB_OFFSET + 96)) count=16 2>/dev/null | sed 's#\x0##g')
+	if [ "$ID" = "$BOARD_ID" ]; then 
     dd if="${IMG_FILE}-second" of="${ID}.dtb" bs=1 skip=$DTB_OFFSET count=$DTB_LEN 2>/dev/null
     echo "Extracted: ${ID}.dtb Size: $(du ${ID}.dtb | awk '{print $1}')Kb Offset: $DTB_OFFSET Length: $DTB_LEN"
+    fi
     done
-rm ${IMG_FILE}-second
+rm -f ${IMG_FILE}-second
 
